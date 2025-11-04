a29ac93e309dd9e94dd5ef88c61b0fa9aed863b2
LeTonVinh <le.tonvinh1@gmail.com>
2025-11-04 20:56:49 +0700

    Add Banner.css for styling banner component with layout and image handling

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<div>Home Page Content</div>} />
-         
+  
         </Route>
       </Routes>
     </BrowserRouter>
--- src/layout/Banner.js
+++ src/layout/Banner.js
@@ -1,8 +1,66 @@
-const Banner = () => {  
+import React from 'react';
+import './Banner.css';
+const Banner = () => {
+    
     return (
-        <div>
-           
-        </div>
+     <div className="banner">
+       <div className="banner-content">
+         <h1>HAT BEAUTIFUL</h1>
+       </div>
+       <div className="banner-images">
+        <marquee behavior="alternate"  scrollamount="5">
+            <img src="/adisdashat.png" alt="Banner 1" className="banner-img" />
+            <img src="/forcehat.png" alt="Banner 2" className="banner-img" />
+            <img src="/munoibanner.png" alt="Banner 3" className="banner-img" />
+            <img src="/non.png" alt="Banner 4" className="banner-img" />
+            <img src="/adisdashat.png" alt="Banner 1" className="banner-img" />
+            <img src="/forcehat.png" alt="Banner 2" className="banner-img" />
+            <img src="/munoibanner.png" alt="Banner 3" className="banner-img" />
+            <img src="/non.png" alt="Banner 4" className="banner-img" />
+            <img src="/adisdashat.png" alt="Banner 1" className="banner-img" />
+            <img src="/forcehat.png" alt="Banner 2" className="banner-img" />
+            <img src="/munoibanner.png" alt="Banner 3" className="banner-img" />
+            <img src="/non.png" alt="Banner 4" className="banner-img" />
+            <img src="/adisdashat.png" alt="Banner 1" className="banner-img" />
+            <img src="/forcehat.png" alt="Banner 2" className="banner-img" />
+            <img src="/munoibanner.png" alt="Banner 3" className="banner-img" />
+            <img src="/non.png" alt="Banner 4" className="banner-img" />
+            <img src="/adisdashat.png" alt="Banner 1" className="banner-img" />
+            <img src="/forcehat.png" alt="Banner 2" className="banner-img" />
+            <img src="/munoibanner.png" alt="Banner 3" className="banner-img" />
+            <img src="/non.png" alt="Banner 4" className="banner-img" />
+            <img src="/adisdashat.png" alt="Banner 1" className="banner-img" />
+            <img src="/forcehat.png" alt="Banner 2" className="banner-img" />
+            <img src="/munoibanner.png" alt="Banner 3" className="banner-img" />
+            <img src="/non.png" alt="Banner 4" className="banner-img" />
+            <img src="/adisdashat.png" alt="Banner 1" className="banner-img" />
+            <img src="/forcehat.png" alt="Banner 2" className="banner-img" />
+            <img src="/munoibanner.png" alt="Banner 3" className="banner-img" />
+            <img src="/non.png" alt="Banner 4" className="banner-img" />
+            <img src="/adisdashat.png" alt="Banner 1" className="banner-img" />
+            <img src="/forcehat.png" alt="Banner 2" className="banner-img" />
+            <img src="/munoibanner.png" alt="Banner 3" className="banner-img" />
+            <img src="/non.png" alt="Banner 4" className="banner-img" />
+            <img src="/adisdashat.png" alt="Banner 1" className="banner-img" />
+            <img src="/forcehat.png" alt="Banner 2" className="banner-img" />
+            <img src="/munoibanner.png" alt="Banner 3" className="banner-img" />
+            <img src="/non.png" alt="Banner 4" className="banner-img" />
+            <img src="/adisdashat.png" alt="Banner 1" className="banner-img" />
+            <img src="/forcehat.png" alt="Banner 2" className="banner-img" />
+            <img src="/munoibanner.png" alt="Banner 3" className="banner-img" />
+            <img src="/non.png" alt="Banner 4" className="banner-img" />
+            <img src="/adisdashat.png" alt="Banner 1" className="banner-img" />
+            <img src="/forcehat.png" alt="Banner 2" className="banner-img" />
+            <img src="/munoibanner.png" alt="Banner 3" className="banner-img" />
+            <img src="/non.png" alt="Banner 4" className="banner-img" />
+            <img src="/adisdashat.png" alt="Banner 1" className="banner-img" />
+            <img src="/forcehat.png" alt="Banner 2" className="banner-img" />
+            <img src="/munoibanner.png" alt="Banner 3" className="banner-img" />
+            <img src="/non.png" alt="Banner 4" className="banner-img" />
+
+        </marquee>
+       </div>
+     </div>
     );
 };
 export default Banner;
