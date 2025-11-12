b6a9071d299accf4176160603f7b1e911c393c17
LeTonVinh <le.tonvinh1@gmail.com>
2025-11-12 11:32:02 +0700

    Fix signup function to remove unused data variable

--- src/Auth/Singup.js
+++ src/Auth/Singup.js
@@ -15,7 +15,7 @@ const Signup = () => {
     setError("");
     setMessage("");
 
-    const { data, error } = await supabase.auth.signUp({
+    const {  error } = await supabase.auth.signUp({
       email,
       password,
     });
